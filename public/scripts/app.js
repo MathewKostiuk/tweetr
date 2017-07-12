31b8c650dc00103bb6a15e846421c87bdf333412
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-07-12 03:38:21 +0000

    sorry! forgot to commit :'P

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,4 +3,49 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+  $('time.timeago').timeago();
+  var tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  }
 
+  var $tweet = createTweetElement(tweetData);
+
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+  function createTweetElement(tweetData) {
+    var timeSince = new Date(tweetData.created_at).toISOString();
+    var $header = $('<header/>').append(
+      $('<h1/>', {text: tweetData.user.name})).append(
+      $('<img/>', {"class": 'tweet-avatar', src: tweetData.user.avatars.small})).append(
+      $('<p/>', {'class': 'tweet-handle', text: tweetData.user.handle}));
+
+    var $body = $('<p/>', {'class': 'tweet-body', text: tweetData.content.text});
+
+    var $icons = $('<span/>', {'class': 'tweet-buttons'}).append(
+      $('<img/>', {id: 'retweet', src: 'https://d30y9cdsu7xlg0.cloudfront.net/png/27744-200.png'})).append(
+      $('<img/>', {id: 'flag', src: 'https://d30y9cdsu7xlg0.cloudfront.net/png/7676-200.png'})).append(
+      $('<img/>', {id: 'love', src: 'https://cdn2.iconfinder.com/data/icons/pittogrammi/142/80-512.png'}));
+
+    var $footer = $('<footer/>').append(
+      $('<time/>', {'class': 'timeago', text: timeSince})).append($icons);
+
+    var $tweet = $('<article/>', {'class': 'tweet-article'}).append($header).append($body).append($footer);
+    return $tweet;
+  }
+
+});
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  var text = $(".new-tweet textarea");
+  var counter = $(".new-tweet span").html();
+
+  text.on('keydown', function(event) {
+    var totalText = $(this).val();
+    var textSubtract = totalText.length;
+    var maxCount = 140;
+
+   $(this).parent().children('.counter').html(counter - textSubtract);
+
+    if (textSubtract >= maxCount) {
+      $(".new-tweet span").addClass('over-limit');
+    } else if (textSubtract <= maxCount) {
+      $(".new-tweet span").removeClass('over-limit');
+    }
+  });
+
+  text.on('keyup', function(event) {
+    var totalText = $(this).val();
+    var textSubtract = totalText.length;
+
+    $(this).parent().children('.counter').html(counter - textSubtract);
+  });
+});
