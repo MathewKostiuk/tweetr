63b922416e13617bec091b0987c5dd2cbb07588b
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-07-12 05:35:03 +0000

    implemented renderTweets function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,8 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  $('time.timeago').timeago();
-  var tweetData = {
+var data = [
+  {
     "user": {
       "name": "Newton",
       "avatars": {
@@ -19,13 +19,48 @@ $(document).ready(function() {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
     "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
   }
-
-  var $tweet = createTweetElement(tweetData);
+];
 
   // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+ // console.log($tweet); // to see what it looks like
+ // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+  function renderTweets(tweets) {
+    tweets.forEach(function(tweetData) {
+      var $tweet = createTweetElement(tweetData);
+      $('#tweets-container').append($tweet);
+    })
+  }
 
   function createTweetElement(tweetData) {
     var timeSince = new Date(tweetData.created_at).toISOString();
@@ -48,4 +83,5 @@ $(document).ready(function() {
     return $tweet;
   }
 
+renderTweets(data);
 });
