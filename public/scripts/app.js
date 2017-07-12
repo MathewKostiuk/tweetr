7d6819bee14d09d3dd5c377cacb5edd8a1022a9e
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-07-12 19:18:46 +0000

    implemented AJAX get && post requests, no longer need driver code =)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,52 +4,32 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-var data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
+
+  function fetchTweets() {
+    $.getJSON('/tweets')
+    .done(function(tweets) {
+      renderTweets(tweets);
+    })
+    // .done(function(tweets) {
+      //
+    // })
   }
-];
+
+  function handleNewTweet(event) {
+    event.preventDefault();
+    var $form = $(this);
+
+    $.ajax({
+      type: 'POST',
+      url: '/tweets',
+      data: $form.serialize()
+    })
+      .done(fetchTweets);
+  }
+
+  var $form = $('#post-tweet');
+
+  $form.on('submit', handleNewTweet);
 
   // Test / driver code (temporary)
  // console.log($tweet); // to see what it looks like
@@ -79,9 +59,11 @@ var data = [
     var $footer = $('<footer/>').append(
       $('<time/>', {'class': 'timeago', text: timeSince})).append($icons);
 
+    $('.timeago').text($.timeago(timeSince));
+
     var $tweet = $('<article/>', {'class': 'tweet-article'}).append($header).append($body).append($footer);
     return $tweet;
   }
 
-renderTweets(data);
+fetchTweets();
 });
